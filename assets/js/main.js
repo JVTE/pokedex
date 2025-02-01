9b2475178427bf94d42459a4738554daf0caf8e9
JVTE <joaovitortorresan@live.com>
2025-02-01 11:18:38 -0300

    Arrumando o CSS e o JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 function convertPokemonToLi(pokemon) {
     return`
-        <li class="pokemon" ${pokemon.type}>
+        <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
                 
@@ -10,7 +10,8 @@ function convertPokemonToLi(pokemon) {
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.photo}" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" 
+                     alt="${pokemon.name}">
             </div>         
         </li>
     `
